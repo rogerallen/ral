aa31845cb6ee31ff4c4a94fbe9f9c7b88c4d02b4
rallen <rallen@gmail.com>
2020-09-14 03:45:13 -0700

    add radians, degrees env functions

--- src/ral.cpp
+++ src/ral.cpp
@@ -399,6 +399,8 @@ void setup_repl_env(std::vector<std::string> args)
     rep("(def! not (fn* (a) (if a false true)))", repl_env);
     rep("(def! load-file (fn* (f) (eval (read-string (str \"(do \" (slurp f) \"\nnil)\")))))", repl_env);
     rep("(defmacro! cond (fn* (& xs) (if (> (count xs) 0) (list 'if (first xs) (if (> (count xs) 1) (nth xs 1) (throw \"odd number of forms to cond\")) (cons 'cond (rest (rest xs)))))))", repl_env);
+    rep("(def! radians (fn* (deg) (* TAU (/ deg 360.))))", repl_env);
+    rep("(def! degrees (fn* (rad) (* 360. (/ rad TAU))))", repl_env);
 }
 
 // this needs more work
